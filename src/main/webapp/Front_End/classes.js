23f834588c3eb15b4db85e080e8677b64f88c029
kryan1622 <krystalryan95@hotmail.co.uk>
2019-06-24 08:57:18 +0100

    refactoring code

--- src/main/webapp/Front_End/classes.js
+++ src/main/webapp/Front_End/classes.js
@@ -21,8 +21,8 @@ function getAllClasses() {
          if (container.rows.length > 1) {
 
              let tableSize = container.rows.length;
-             for (i = tableSize; i > 1; i--) {
-                 container.deleteRow(i - 1);
+             for (let j = tableSize; j > 1; j--) {
+                 container.deleteRow(j - 1);
              }
 
         }
